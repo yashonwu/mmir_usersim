cb14b8acdf32eb558f49e226f9a5d690ad448bc5
yashonwu <apfelwu@qq.com>
2023-03-24 18:33:36 +0000

    add captioning

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='captioning',
+    version='0.0.1',
+    description='User simulator with relative captioning.',
+    author='yashon',
+    author_email='apfelwu@gmail.com',
+    url='https://github.com/yashonwu',
+    packages=['captioning', 'captioning.data', 'captioning.models', 'captioning.utils'],
+)
+
